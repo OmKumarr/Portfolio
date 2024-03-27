8e2f1edca61bf5bf0cc63d8418d837f4d380ad44
Om Kumar <kumarchaudhary17427@gmail.com>
2024-03-27 10:26:43 +0545

    Web

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//Typing Animation 
+var typed = new Typed(".auto-type", {
+  strings: ["WordPress Devloper","Graphics Designer", "Web Devloper" ],
+  typeSpeed : 150,
+  backspeed : 150,
+  loop : true
+});
+
+// Slider Animation
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+// Progress Bar
